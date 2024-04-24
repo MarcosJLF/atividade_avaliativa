545eab3a0608577f371224f7bac4924dd03ef6ec
MarcosJLF <163335470+MarcosJLF@users.noreply.github.com>
2024-04-24 17:08:33 -0300

    questão03.js

--- atividade01/questão03.js
+++ atividade01/questão03.js
@@ -1,17 +1,19 @@
 // 3) Calcular a soma dos números de um array
 
+function soma() {
 
+  let numberlist = [];
 
-function soma (){
+  let a = +prompt('')
 
-    let numberlist = [1,2,3,4,5];
-    let couter = 0;
+  for (let i = 0; i < a; i++) {
+    numberlist.push(+prompt('Digite um número'))
+  }
+  let couter = 0;
+  for (let i = 0; i < numberlist.length; i++) {
+    couter += numberlist[i]
+  }
+  console.log(couter)
 
-    for(let i = 0; i < numberlist.length; i++){
-        couter += numberlist[i]
-    }
-    console.log(couter)
-    
 }
 soma()
-
