60f56fea60876704030660c796b0f2b631bec8f5
MarcosJLF <163335470+MarcosJLF@users.noreply.github.com>
2024-04-24 18:58:33 -0300

    questão03.js

--- atividade01/questão03.js
+++ atividade01/questão03.js
@@ -1,11 +1,8 @@
 // 3) Calcular a soma dos números de um array
 
 function soma() {
-
   let numberlist = [];
-
-  let a = +prompt('')
-
+  let a = +prompt('Digite a quantidade de número que será somado:')
   for (let i = 0; i < a; i++) {
     numberlist.push(+prompt('Digite um número'))
   }
@@ -13,7 +10,13 @@ function soma() {
   for (let i = 0; i < numberlist.length; i++) {
     couter += numberlist[i]
   }
-  console.log(couter)
-
+  console.log(numberlist)
+  console.log(couter) 
 }
 soma()
+
+/*O que foi alterado:
+Adicionei um loop que pergunta quantos números ele deseja adicionar a uma array, 
+e depois soma essa array. O código já estava pronto; apenas adicionei essa nova funcionalidade, 
+pois ele pegava alguns números já definidos e os somava.
+ */
