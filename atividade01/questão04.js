c5f3ed999881564d384fadf3af0f4763e8697007
MarcosJLF <163335470+MarcosJLF@users.noreply.github.com>
2024-04-24 17:14:03 -0300

    questão04.js

--- atividade01/questão04.js
+++ atividade01/questão04.js
@@ -3,33 +3,24 @@ Matemática. Calcular a média de todas as notas acima de 5,0.
 */
 
 function grades() {
-    
-    let m = 0;
-    let grades = 0;
-    let q = +prompt('Digite o número de notas')
-    let numberlist = []
-    let counter = 0;
-  
-    for (let i = 0; i < q; i++) {
-        prompt('Digite um nome')
-      grades = +prompt('Digite a grade')
-      numberlist[i] = grades
-  
-      if (numberlist[i] > 5) {
-        counter += numberlist[i]
-        m = counter / q
-  
-      }
-  
+
+  let m = 0;
+  let grades = 0;
+  let q = +prompt('Digite o número de notas')
+  let numberlist = []
+  let counter = 0;
+  let nome = prompt('Digite o nome do aluno')
+
+  for (let i = 0; i < q; i++) {
+    grades = +prompt('Digite a grade')
+    numberlist[i] = grades
+
+    if (numberlist[i] > 5) {
+      counter += numberlist[i]
+      m = counter / q
     }
-    alert( `Média ${m}` )    
 
- 
   }
-  
- grades()
-
-
-
-
-  
+  alert(`Média ${m}\nAluno ${nome}`)
+}
+grades()
