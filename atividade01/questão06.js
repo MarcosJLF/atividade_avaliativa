10c6d4d410cd3e1f0be6cee582d1240614a00e20
MarcosJLF <163335470+MarcosJLF@users.noreply.github.com>
2024-04-25 10:41:29 -0300

    questão06.js

--- atividade01/questão06.js
+++ atividade01/questão06.js
@@ -1,22 +1,17 @@
 /* 6) Crie 2 Arrays, no primeiro você pedirá para o usuário digitar 10 números.
 Faça um código para copiar para o segundo Array, apenas os números ímpares existentes
 no primeiro Array.
-*/ 
-
-function arrays (){
-
-let list = [];
-let m = 0;
-
-for(let i = 0; i < 10; i++){
-     m = +prompt("Digite dez números")
-    if(m % 2 != 0){
-        console.log(m)
-        list.push(m)
+*/
+function arrays() {
+  let list = [];
+  let m = 0;
+  for (let i = 0; i < 10; i++) {
+    m = +prompt("Digite dez números " + (i + 1) + " /10")
+    if (m % 2 != 0) {
+      list.push(m)
     }
+  }
+  alert(list)
 }
-alert(list)
-
-}
-
 arrays()
+
