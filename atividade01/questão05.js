dfbd48196af56417751f1a2e4cffa83117a5318e
MarcosJLF <163335470+MarcosJLF@users.noreply.github.com>
2024-04-25 11:12:56 -0300

    questão05.js

--- atividade01/questão05.js
+++ atividade01/questão05.js
@@ -1,22 +1,36 @@
 // 5) Localizar o maior valor dentro de um array de números
 
+function array() {
 
-
-function array (){
-
-let number = [1,10,3,4,5]
-let maior = number[0]
-
-
-for(let i = 0; i < number.length; i++){
-    if(number[i] > maior ){
-        maior = number[i]
-    }
+  let numberarray = +prompt('Quantos números deseja adicionar à array?');
+  
+  let arrays = [];
+  
+  for (let i = 0; i < numberarray; i++) {
+  
+    arrays.push(+prompt('Digite um número ' + (i + 1) + `/${numberarray}`))
     
+  }
+  console.log(arrays)
+  
+  let maior = arrays[0]
+  
+  for (let i = 0; i < arrays.length; i++) {
+  
+    if (arrays[i] > maior) {
+    
+      maior = arrays[i]
+      
+    }
+  }
+  console.log(maior)
+  
+  alert('O maior número é: '+maior)
+  
 }
-console.log(maior)
-
-}
-
-
 array()
+
+//Eu apenas modifiquei o código para perguntar ao usuário quantos números ele deseja adicionar em um array, 
+//e adicionei um contador. A função não era necessária; eu poderia removê-la, mas preferi não mudar muito meu código, 
+//apenas adicionar novas funcionalidades para torná-lo mais interativo.
+    
